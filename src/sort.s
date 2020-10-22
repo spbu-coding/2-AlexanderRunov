a48ab01916c1fc248f77d3b0686f98948b2a3b82
Alexander Runov <st087438@student.spbu.ru>
2020-10-22 20:06:41 +0300

    new file:   src/main.c 	new file:   src/sort.s

---
+++ src/sort.s
@@ -0,0 +1,101 @@
+	.file	"sort.c"
+	.text
+	.globl	sort
+	.type	sort, @function
+sort:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	$0, -12(%rbp)
+	jmp	.L2
+.L6:
+	movl	-12(%rbp), %eax
+	addl	$1, %eax
+	movl	%eax, -8(%rbp)
+	jmp	.L3
+.L5:
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movq	(%rax), %rdx
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rcx
+	movq	-24(%rbp), %rax
+	addq	%rcx, %rax
+	movq	(%rax), %rax
+	cmpq	%rax, %rdx
+	jle	.L4
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movq	(%rax), %rax
+	movl	%eax, -4(%rbp)
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	-12(%rbp), %edx
+	movslq	%edx, %rdx
+	leaq	0(,%rdx,8), %rcx
+	movq	-24(%rbp), %rdx
+	addq	%rcx, %rdx
+	movq	(%rax), %rax
+	movq	%rax, (%rdx)
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	cltq
+	movq	%rax, (%rdx)
+.L4:
+	addl	$1, -8(%rbp)
+.L3:
+	movl	-8(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L5
+	addl	$1, -12(%rbp)
+.L2:
+	movl	-12(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L6
+	nop
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sort, .-sort
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
